e316374531459d4e5b7707299f7c354b526149fb
jschlitt1 <jschlitt1@my.wctc.edu>
2019-04-25 15:13:56 -0500

    trying to create view
    currently not working

---
+++ Controllers/CartController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Northwind.Controllers
+{
+    public class CartController : Controller
+    {
+        public ActionResult CartList() => View();
+    }
+}
