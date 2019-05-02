d73ca49cde842e677c7e238e6d865f61a4dfe6ef
Jessie <jschlitt1@my.wctc.edu>
2019-05-02 12:59:14 -0500

    cart update
    display cart according to logged in user

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -14,6 +15,33 @@ namespace Northwind.Controllers
         private INorthwindRepository repository;
         public CartController(INorthwindRepository repo) => repository = repo;
 
-        public ActionResult CartList() => View(repository.CartItems.Include("Product"));
+        public ActionResult CartList()
+        {
+            int customerId = repository.Customers.FirstOrDefault(c => c.Email == User.Identity.Name).CustomerID;
+            return View(repository.CartItems.Include("Product").Where(u => u.CustomerId == customerId));
+        }
+
+        //[HttpPost]
+        //public async Task<IActionResult> Delete(string id)
+        //{
+        //    AppUser user = await userManager.FindByIdAsync(id);
+        //    if (user != null)
+        //    {
+        //        IdentityResult result = await userManager.DeleteAsync(user);
+        //        if (result.Succeeded)
+        //        {
+        //            return RedirectToAction("Index");
+        //        }
+        //        else
+        //        {
+        //            AddErrorsFromResult(result);
+        //        }
+        //    }
+        //    else
+        //    {
+        //        ModelState.AddModelError("", "User Not Found");
+        //    }
+        //    return View("Index", userManager.Users);
+        //}
     }
 }
